6e740b09ca0eda975efe5abd960f26dafc383b28
KhanhLPBao <khanhlpbao@gmail.com>
2023-07-28 16:39:23 +0700

    new file:   admin/process_begin.sh 	deleted:    admin/processing.sh

---
+++ admin/process_begin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+signaldir="media/signal"
+storagedir="media/storage"
+logdir="log/processing"
+linkdir=$storagedir"/link"
+workingdir=$signaldir"/working/"
+processdir=$signaldir"/processing"
+sessiondir=$signaldir"/sessions"
+errordir=$storagedir"/error"
+statusdir=$storagedir"/status"
+
